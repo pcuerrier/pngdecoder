09b6ea4dc99a4f9db20b3a6e3950c26062f93728
Philippe Cuerrier <philippe.cuerrier@gmail.com>
2022-11-21 14:10:34 -0500

    Switched back to C++17

--- test/premake5.lua
+++ test/premake5.lua
@@ -1,7 +1,7 @@
 project "test"
     kind "ConsoleApp"
     language "C++"
-    cppdialect "C++20"
+    cppdialect "C++17"
 
     targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
